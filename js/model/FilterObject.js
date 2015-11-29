497c69f97e354bf5564eaedbbd957fd6fe67c750
Tyler Hastings <tyler@neadwerx.com>
2015-11-29 13:03:36 -0500

    adding my code back.. not sure how it was deleted again.

--- js/model/FilterObject.js
+++ js/model/FilterObject.js
@@ -5,6 +5,7 @@ var FilterObject = function ( minDate, maxDate, minPoints, maxPoints, positions 
     this.minPoints = minPoints;
     this.maxPoints = maxPoints;
     this.positions = positions;
+    this.players = [];
 };
 
 FilterObject.prototype.toString = function ()
@@ -34,4 +35,33 @@ FilterObject.prototype.prettyPrint = function ()
 
     return stringVal;
 
+};
+
+FilterObject.prototype.generateCareerUrl = function ()
+{
+    //Base URL
+    var url = 'http://localhost:8000/careers/?';
+
+    //Boolean to check and see if we should use ? or & to add new params
+    var addAmp = '';
+
+    if( this.minDate !== null )
+    {
+        url += "start_year=" + this.minDate + "&start_year_end=" + this.maxDate;
+        addAmp = '&';
+    }
+
+    if( this.minPoints !== null )
+    {
+        url += addAmp + 'min_pts=' + this.minPoints + '&max_pts=' + this.maxPoints;
+        addAmp = '&';
+    }
+
+    if( this.positions !== null && this.positions.length > 0 )
+    {
+        url += addAmp + 'pos=' + this.positions.join('&pos=');
+        addAmp = '&';
+    }
+
+    return url;
 };
