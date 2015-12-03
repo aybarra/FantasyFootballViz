09bc4db2bf8217371ed36642da576add302d7da1
Andy Ybarra <andy.ybarra@gatech.edu>
2015-12-02 22:31:46 -0500

    Added d3 version of cdf chart

--- js/cdf_d3.js
+++ js/cdf_d3.js
@@ -1,14 +1,5 @@
 function generateCDF_D3Chart(){
   
-  var cdf_margin = { top: 10, right: 30, bottom: 33, left: 45 };
-  var cdf_width = 700 - cdf_margin.right - cdf_margin.left;
-  var cdf_height = 400 - cdf_margin.top - cdf_margin.bottom;
-  // var margin = {top: 20, right: 80, bottom: 30, left: 50},
-  //   width = 960 - margin.left - margin.right,
-  //   height = 500 - margin.top - margin.bottom;
-
-  // var parseDate = d3.time.format("%Y%m%d").parse;
-
   var x = d3.time.scale().range([0, cdf_width]);
   var y = d3.scale.linear().range([cdf_height, 0]);
 
@@ -24,45 +15,39 @@ function generateCDF_D3Chart(){
 
   var line = d3.svg.line()
       .interpolate("basis")
-      .x(function(d) { return x(d.date); })
-      .y(function(d) { return y(d.ff_pts); });
+      .x(function(d) { return x(d.x); })
+      .y(function(d) { return y(d.y); });
 
   var svg = d3.select("#cdf_chart").append("svg")
       .attr("width", cdf_width + cdf_margin.left + cdf_margin.right)
       .attr("height", cdf_height + cdf_margin.top + cdf_margin.bottom)
-    .append("g")
+      .append("g")
       .attr("transform", "translate(" + cdf_margin.left + "," + cdf_margin.top + ")");
 
 
   d3.json('http://localhost:8000/seasons_subset/', function(error,data){
     if (error) throw error;
 
-    color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
+    var data_conv = convertData(data);
 
-    // data.forEach(function(d) {
-    //   d.date = parseDate(d.date);
-    // });
+    var keys = d3.keys(data_conv);
+    color.domain(d3.keys(data_conv));
 
-    var cities = color.domain().map(function(name) {
-      return {
-        name: name,
-        values: data.map(function(d) {
-          return {date: d.date, temperature: +d[name]};
-        })
-      };
-    });
-
-    x.domain(d3.extent(data, function(d) { return d.x; }));
+    x.domain([
+      0,
+      d3.max(data_conv, function(d) { return d.values.length; })  // Want the longest length career
+    ]);
 
     y.domain([
-      d3.min(cities, function(c) { return d3.min(c.values, function(v) { return v.temperature; }); }),
-      d3.max(cities, function(c) { return d3.max(c.values, function(v) { return v.temperature; }); })
+      d3.min(data_conv, function(d) { return d3.min(d.values, function(v) { return v.y; }); }),
+      d3.max(data_conv, function(d) { return d3.max(d.values, function(v) { return v.y; }); })
     ]);
 
     svg.append("g")
         .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+        .attr("transform", "translate(0," + cdf_height + ")")
+        .call(xAxis)
+        .text("Years Played");
 
     svg.append("g")
         .attr("class", "y axis")
@@ -72,135 +57,36 @@ function generateCDF_D3Chart(){
         .attr("y", 6)
         .attr("dy", ".71em")
         .style("text-anchor", "end")
-        .text("Temperature (ºF)");
+        .text("Cumulative Fantasy Points");
 
-    var city = svg.selectAll(".city")
-        .data(cities)
+    var player = svg.selectAll(".player")
+        .data(data_conv)
       .enter().append("g")
-        .attr("class", "city");
+        .attr("class", "player");
 
-    city.append("path")
+    player.append("path")
         .attr("class", "line")
         .attr("d", function(d) { return line(d.values); })
-        .style("stroke", function(d) { return color(d.name); });
+        .style("stroke", function(d) { return color(d.key); });
 
-    city.append("text")
-        .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-        .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
+    player.append("text")
+        .datum(function(d) { return {name: d.key, value: d.values[d.values.length - 1]}; })
+        .attr("transform", function(d) { return "translate(" + x(d.value.x) + "," + y(d.value.y) + ")"; })
         .attr("x", 3)
         .attr("dy", ".35em")
         .text(function(d) { return d.name; });
   });
-
-  // loadCDFChart();
 }
 
-// function loadCDFChart()
-// {
-//     // var playerArray = filteredPlayers();
-//     d3.json('http://localhost:8000/seasons_subset/', function(error,data){
-//       data_conv = convertData(data);
-//       console.log(data_conv);
-//       data_conv.forEach( function (x)
-//       {
-//           x.short_year = x.start_year;
-//           x.year = yearFormat.parse( x.start_year + "" );
-//           x.name = x.player_name;
-//           x.pos = x.pos_type;
-//           {
-//             if (x.pos == "qb"){x.pos_long = "Quarterback"}
-//             else if(x.pos == "rb"){x.pos_long = "Running Back"}
-//             else if(x.pos == "wr"){x.pos_long = "Wide Receiver"}
-//             else if(x.pos == "te"){x.pos_long = "Tight End"}
-//             else {x.pos == ""}
-//           };
-//           x.fantPt = +x.ff_pts;
-//           x.winPct = d3.round( (+x.win_pct * 100), 2 );
-//       } );
-
-//       // Scale the domain of the data
-//       var xmin = d3.min(playerArray, function(d) { return d.winPct; }) - scat_xaxis_padding,
-//           xmax = d3.max(playerArray, function(d) { return d.winPct; }) + scat_xaxis_padding,
-//           ymin = d3.min(playerArray, function(d) { return d.fantPt; }) - scat_yaxis_padding,
-//           ymax = d3.max(playerArray, function(d) { return d.fantPt; }) + scat_yaxis_padding;
 
 
-//       x.domain([xmin, xmax]);
-//       y.domain([ymin, ymax]);
-
-//       // Add the scatterplot
-//       svg_scatter.selectAll( "dot" )
-//               .data( playerArray )
-//               .enter().append( "circle" )
-//               .attr( "r", 3 )
-//               .attr( "cx", function ( d )
-//               {
-//                   return x( d.winPct );
-//               } )
-//               .attr( "cy", function ( d )
-//               {
-//                   return y( d.fantPt );
-//               } )
-//               .attr( "id", function ( d, i )
-//               {
-//                   return "dot_" + i;
-//               } ) // added
-//               .attr( "class", "dot" )
-//               .style( "fill", function ( d )
-//               {
-//                   return colorScale( d.pos );
-//               } )
-          
-//               .on( "mouseover", function ( d )
-//               {
-//                   d3.select( this )
-//                           .attr( "stroke", "black" )
-//                           .attr( "stroke-width", 2 )
-//                           .attr( "fill-opacity", 1 );
-//                   tooltip
-//                   .style("left", (d3.event.pageX + 5) + "px")
-//                   .style("top", (d3.event.pageY - 5) + "px")
-//                   .transition().duration(300)
-//                   .style("opacity", 1)
-//                   .style("display", "block")
-//                   // tooltip.html( "Name: " + d.name + "<br/>" +
-//                   //         "Position: " + d.pos + "<br/>" +
-//                   //         "Fantasy Pts: " + d.fantPt + "<br/>" +
-//                   //         "Win %: " + d.winPct )
-//                   //         .style( "left", (d3.event.pageX + 5) + "px" )
-//                   //         .style( "top", (d3.event.pageY - 5) + "px" )
-//                   //         .transition().duration( 300 )
-//                   //         .style( "opacity", 1 )
-//                   //         .style( "display", "block" );
-//                   updateDetails(d);
-//               } )
-//               .on( "mouseout", function ( d )
-//               {
-//                   d3.select( this )
-//                           .attr( "stroke", "" )
-//                           .attr( "fill-opacity", function ( d )
-//                           {
-//                               return 1;
-//                           } )
-
-//                   tooltip.transition().duration( 700 ).style( "opacity", 0 );
-//               } );
-
-//       // Add the X Axis
-//       svg_scatter.append( "g" )
-//               .attr( "class", "x axis" )
-//               .attr( "transform", "translate(0," + scatter_height + ")" )
-//               .call( xAxis );
-
-//       // Add the Y Axis
-//       svg_scatter.append( "g" )
-//               .attr( "class", "y axis" )
-//               .call( yAxis );
-
-//       lasso.items( d3.selectAll( ".dot" ) );
-//     });
-// }
-
+// Returns an array of objects of the form:
+//  [{
+//      key: pguid
+//      values: arr [ x <-- year, 
+//                    y <-- ff_pts (cumulative)]
+//            
+//  },...]    
 function convertData(data){
   var lines = {};
   var plot_data = [];
